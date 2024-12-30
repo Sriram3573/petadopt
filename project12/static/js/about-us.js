ce5c6e2a2a15e8c65217c4b50f5c316602279db1
Sriram S <161583945+Sriram3573@users.noreply.github.com>
2024-12-31 01:07:19 +0530

    Delete project12 directory

--- project12/static/js/about-us.js
+++
@@ -1,7 +0,0 @@
-// Display an additional message on double-click
-document.querySelector(".about").addEventListener("dblclick", () => {
-    alert(
-      "Thank you for your interest! We are always working to find loving homes for every pet in need."
-    );
-  });
-  
