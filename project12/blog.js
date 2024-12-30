1310f125be418658fd1326d7db529685f5cfda83
Sriram S <161583945+Sriram3573@users.noreply.github.com>
2024-12-30 19:40:27 +0530

    Delete project12 directory

--- project12/blog.js
+++
@@ -1,8 +0,0 @@
-const blogPosts = document.querySelectorAll(".blog-post");
-
-blogPosts.forEach(post => {
-  post.addEventListener("click", () => {
-    const title = post.querySelector("h3").textContent;
-    alert(`You clicked on: ${title}. Full post coming soon!`);
-  });
-});
