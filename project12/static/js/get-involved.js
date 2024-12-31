ecb70015b96fafc75e55a96ed67220f544300ade
Sriram S <161583945+Sriram3573@users.noreply.github.com>
2024-12-31 12:55:01 +0530

    Delete project12 directory

--- project12/static/js/get-involved.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".get-involved button").addEventListener("click", () => {
-    alert("Thank you for your interest! Visit our Volunteer and Foster pages to get started.");
-  });
-  
